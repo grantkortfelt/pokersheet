4c62239ace16f6fd030a2e983dd373a5090c2c57
grantkortfelt <grantmail3301@gmail.com>
2025-02-14 13:49:57 +1300

    Create webpage

--- python/database.py
+++ python/database.py
@@ -16,6 +16,16 @@ def add_player(name):
     conn.commit()
     conn.close()
 
+def update_player(name):
+    conn = sqlite3.connect('poker.db')
+    cur = conn.cursor()
+    cur.execute("SELECT net FROM play WHERE name=?", (name,))
+    net = sum(x[0] for x in cur.fetchall())
+    print(net)
+    cur.execute("UPDATE player SET net=? WHERE name=?", (net, name))
+    conn.commit()
+    conn.close()
+
 def add_session(month, day, buyin):
     conn = sqlite3.connect('poker.db')
     cur = conn.cursor()
@@ -23,9 +33,12 @@ def add_session(month, day, buyin):
     conn.commit()
     conn.close()
 
-def add_play(name, session, net, buyins, cash):
+def add_play(name, session, buyins, cash):
     conn = sqlite3.connect('poker.db')
     cur = conn.cursor()
+    cur.execute("SELECT buyin FROM session WHERE id=?", (session,))
+    buyin = float(cur.fetchall()[0][0])
+    net = cash - buyin * buyins
     cur.execute("INSERT INTO play (name, session, net, buyins, cash) VALUES (?, ?, ?, ?, ?)", (name, session, net, buyins, cash))
     conn.commit()
     conn.close()
@@ -34,7 +47,7 @@ def get_player(name):
     conn = sqlite3.connect('poker.db')
     cur = conn.cursor()
     cur.execute("SELECT * FROM player WHERE name=?", (name,))
-    player = cur.fetchone()
+    player = cur.fetchall()
     conn.close()
     return player
 
@@ -53,3 +66,20 @@ def get_play(name, session):
     play = cur.fetchone()
     conn.close()
     return play
+
+
+def drop_tables():
+    conn = sqlite3.connect('poker.db')
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS player")
+    cur.execute("DROP TABLE IF EXISTS session")
+    cur.execute("DROP TABLE IF EXISTS play")
+    conn.commit()
+    conn.close()
+
+update_player('Alice')
+update_player('Bob')
+update_player('Charlie')
+print(get_player('Alice'))
+print(get_player('Bob'))
+print(get_player('Charlie'))
